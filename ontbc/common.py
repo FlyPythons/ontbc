cafe3fab887048b37f53c63eb2a2068404b7986c
FlyPythons <schooiboy@qq.com>
2018-12-11 12:46:50 +0800

    move readfq to common.py

--- ontbc/common.py
+++ ontbc/common.py
@@ -29,6 +29,51 @@ def read_tsv(file, sep=None):
         yield line.split(sep)
 
 
+def readfq(file):  # this is a generator function
+    """
+    modified from "https://github.com/lh3/readfq"
+    :param file: filename
+    :return:
+    """
+    if file.endswith(".gz"):
+        fp = gzip.open(file)
+    elif file.endswith(".fastq") or file.endswith(".fq"):
+        fp = open(file)
+    else:
+        raise Exception("%r file format error" % file)
+
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].partition(" ")[0], [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, ''.join(seqs), None # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, seq, ''.join(seqs); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, seq, None # yield a fasta record instead
+                break
+
+
+
 def n50(lengths):
     """
     return N50 of lengths
--- ontbc/filter.py
+++ ontbc/filter.py
@@ -4,7 +4,6 @@
 import sys
 import argparse
 import logging
-import gzip
 import os.path
 
 import matplotlib
@@ -12,55 +11,11 @@ matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 
 from ontbc.parser import add_filter_parser
-from ontbc.common import read_tsv, n50
+from ontbc.common import read_tsv, n50, readfq
 from ontbc import __author__, __email__, __version__
 
 
 LOG = logging.getLogger(__name__)
-
-
-def readfq(file):  # this is a generator function
-    """
-    modified from "https://github.com/lh3/readfq"
-    :param file: filename
-    :return:
-    """
-    if file.endswith(".gz"):
-        fp = gzip.open(file)
-    elif file.endswith(".fastq") or file.endswith(".fq"):
-        fp = open(file)
-    else:
-        raise Exception("%r file format error" % file)
-
-    last = None # this is a buffer keeping the last unprocessed line
-    while True: # mimic closure; is it a bad idea?
-        if not last: # the first record or a record following a fastq
-            for l in fp: # search for the start of the next record
-                if l[0] in '>@': # fasta/q header line
-                    last = l[:-1] # save this line
-                    break
-        if not last: break
-        name, seqs, last = last[1:].partition(" ")[0], [], None
-        for l in fp: # read the sequence
-            if l[0] in '@+':
-                last = l[:-1]
-                break
-            seqs.append(l[:-1])
-        if not last or last[0] != '+': # this is a fasta record
-            yield name, ''.join(seqs), None # yield a fasta record
-            if not last: break
-        else: # this is a fastq record
-            seq, leng, seqs = ''.join(seqs), 0, []
-            for l in fp: # read the quality
-                seqs.append(l[:-1])
-                leng += len(l) - 1
-                if leng >= len(seq): # have read enough quality
-                    last = None
-                    yield name, seq, ''.join(seqs); # yield a fastq record
-                    break
-            if last: # reach EOF before reading enough quality
-                yield name, seq, None # yield a fasta record instead
-                break
 
 
 def get_summary(summary):
